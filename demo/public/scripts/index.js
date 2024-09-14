347d9d398561e51d71d9af194ecb0f26372860b6
yotu <114195789+ulxsth@users.noreply.github.com>
2024-09-14 10:33:16 +0900

    chore: css 忘れてた

--- demo/public/scripts/index.js
+++ demo/public/scripts/index.js
@@ -2,9 +2,6 @@ import io from "socket.io-client";
 import Ace from "ace-builds";
 
 const editor = Ace.edit("editor");
-editor.setTheme("ace/theme/chrome");
-editor.session.setMode("ace/mode/markdown");
-
 const socket = io();
 
 // イベントの受信
