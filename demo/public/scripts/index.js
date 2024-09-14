5919ebf08160e92d81922f7dc45be48918f7bca9
yotu <114195789+ulxsth@users.noreply.github.com>
2024-09-14 09:05:51 +0900

    chore: 移植

---
+++ demo/public/scripts/index.js
@@ -0,0 +1,15 @@
+const editor = ace.edit("editor");
+editor.setTheme("ace/theme/chrome");
+editor.session.setMode("ace/mode/markdown");
+
+const socket = io();
+
+// イベントの受信
+socket.on("change", (delta) => {
+
+});
+
+// イベントの発信
+editor.session.on('change', (delta) => {
+  console.log(editor.session.doc.getValue().length);
+});
--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,1 +1,29 @@
-import { AceAdapter } from "@nemurusleepy/either";
+import express from 'express'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import { EditorSocketIOServer } from '@nemurusleepy/either'
+
+const app = express()
+app.use(express.static('public'))
+
+const server = createServer(app)
+const io = new Server(server)
+
+const editorSocketIOServer = new EditorSocketIOServer("channel1", "", [])
+
+app.get("/", (req, res) => {
+  return express.static('public/index.html')
+})
+
+
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id)
+  editorSocketIOServer.addClient(socket)
+})
+
+const port = 3000
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+}).on('error', (err) => {
+  throw new Error(err.message)
+})
