22142d61a7c04cdaf1114b3672724a08a79ccf78
yotu <114195789+ulxsth@users.noreply.github.com>
2024-09-15 20:17:29 +0900

    feat: transform ins/ins パターンの解決

--- either-client/src/index.js
+++ either-client/src/index.js
@@ -80,124 +80,50 @@ export class AceAdapter {
   };
 
   /**
-   * ふたつの差分を受け取り、競合しないように変換して返す。
-   * 返されるdeltaは、実行順に並べたもの。
-   * @param {*} delta1
-   * @param {*} delta2
-   * @returns [delta1, delta2?] 実行順。delta2はnullの場合がある。
+   * 他者が変更した差分を受け取り、最新の変更に対して競合しないように変換して返す。
+   * @param {*} targetDelta 対象の差分
+   * @returns delta 競合しないように変換された差分
    */
-  transform(delta1, delta2) {
-    if (!delta1) {
-      return [delta2];
-    } else if (!delta2) {
-      return [delta1];
-    }
+  transform(targetDelta) {
+    const latestDelta = this.deltas.pop();
 
     // insert / insert
-    // ふたつのdeltaの開始位置が違う行に対して存在している場合、より下の行を編集するdeltaを優先する。
-    // ふたつのdeltaの開始位置が違う列に対して存在している場合、より右の行を編集するdeltaを優先する。
-    // ふたつの開始位置が完全に同じ場合、後者のdeltaの開始位置を前者の終了位置に合わせる。
-    if (delta1.action === "insert" && delta2.action === "insert") {
-      if (delta1.start.row < delta2.start.row) {
-        let w = delta1;
-        delta1 = delta2;
-        delta2 = w;
-      } else if (delta1.start.row === delta2.start.row) {
-        if (delta1.start.column < delta2.start.column) {
-          let w = delta1;
-          delta1 = delta2;
-          delta2 = w;
-        } else if (delta1.start.column === delta2.start.column) {
-          delta2.start = delta1.end;
+    if (latestDelta.action === "insert" && targetDelta.action === "insert") {
+      // latest より後の行の場合、latest が追加する行の分開始位置の行をずらす
+      if (latestDelta.start.row < targetDelta.start.row) {
+        const rowDelta = latestDelta.lines.length - 1;
+        targetDelta.start.row += rowDelta;
+        targetDelta.end.row += rowDelta;
+        return targetDelta;
 
-          const d = this.calcMoveDelta(delta1.start, delta1.end);
-          delta2.end = this.calcMovedPosition(delta2.end, d);
-        }
-      }
-    }
+      // latest と同じ行の場合、編集する列を比較する必要がある
+      } else if (latestDelta.start.row === targetDelta.start.row) {
+        // latest より後の列の場合
+        // 行：latest が追加する行の分ずらす
+        // 列：改行しない場合は追加する列の分、する場合はそこから改行前の文字数分左に移動する必要がある
+        if (latestDelta.start.column < targetDelta.start.column) {
+          const rowDelta = latestDelta.lines.length - 1;
+          targetDelta.start.row += rowDelta;
+          targetDelta.end.row += rowDelta;
+          targetDelta.start.column += latestDelta.lines[0].length;
+          targetDelta.end.column += latestDelta.lines[0].length;
+          if (latestDelta.lines.length > 1) {
+            const columnDelta = targetDelta.start.column - latestDelta.start.column;
+            targetDelta.start.column -= columnDelta;
+            targetDelta.end.column -= columnDelta;
+          }
 
-    // remove / remove
-    // 削除は範囲に対して行うため、ふたつの範囲のとりえる状態によって処理を分ける。
-    if (delta1.action === "remove" && delta2.action === "remove") {
-      // 0. 範囲が同一の場合、2つめのDeltaを無効化する。
-      if (delta1.start.row === delta2.start.row && delta1.start.column === delta2.start.column) {
-        return [delta1];
-      }
-
-      // 1. 行範囲が交差しない場合
-      //   ふたつのdeltaの行範囲が交差しないか一行だけ重複する場合、より後のdeltaを優先する。
-      if (delta1.end.row < delta2.start.row
-        || delta1.end.row === delta2.start.row
-        && delta1.end.column < delta2.start.column
-      ) {
-        let w = delta1;
-        delta1 = delta2;
-        delta2 = w;
-
-        // 2. 行範囲が交差し、かつ列範囲が交差する場合
-        //   削除は範囲に対して行うため、ふたつの範囲のとりえる状態によって処理を分ける。
-      } else if (this.hasDuplicate(delta1, delta2)) {
-
-        //   - 包含：より大きい範囲が小さい範囲を包含する場合、小さい範囲を削除する。2つめのDeltaを無効化する。
-        if (this.isDeltaIncludeDelta(delta1, delta2)) {
-          return [delta1];
-        } else if (this.isDeltaIncludeDelta(delta2, delta1)) {
-          return [delta2];
-
-          //   - 交差：範囲が重なる場合、ふたつの範囲をマージした範囲を作成する。2つめのDeltaは無効化する。
+        // 編集する行が同じ / latest より前の列の場合、変換の必要はない
         } else {
-          // TODO: mergeDeltas
-          const newDelta = this.mergeDeltas(delta1, delta2);
-          return [newDelta];
+          return targetDelta;
         }
+
+      // latest より前の行の場合、変換の必要はない
+      } else {
+        return targetDelta;
       }
     }
-
-    // insert / remove
-    // 挿入は開始位置、削除は範囲として扱うため、ふたつのdeltaのとりえる状態によって処理を分ける。
-    if (delta1.action === "insert" && delta2.action === "remove") {
-      // 0. 挿入位置が削除範囲の前にある場合、適用の順番を入れ替える。
-      if (delta1.start.row < delta2.start.row
-        || delta1.start.row === delta2.start.row
-        && delta1.start.column < delta2.start.column
-      ) {
-        let w = delta1;
-        delta1 = delta2;
-        delta2 = w;
-
-        // 1. 削除範囲の中に挿入位置がある場合、削除位置の開始地点に挿入位置をずらす。
-      } else if (this.isDeltaIncludePosition(delta2, delta1.start)) {
-        const d = this.calcMoveDelta(delta2.start, delta1.start);
-        delta1.start = delta2.start;
-        delta1.end = this.calcMovedPosition(delta1.end, d);
-      }
-
-      // 2. 挿入位置が削除範囲の後ろにある場合、そのまま返す。
-    }
-
-    if (delta1.action === "remove" && delta2.action === "insert") {
-      // 0. 削除範囲の前に挿入位置がある場合、適用の順番を入れ替える。
-      if (delta2.start.row < delta1.start.row
-        || delta2.start.row === delta1.start.row
-        && delta2.start.column < delta1.start.column
-      ) {
-        let w = delta1;
-        delta1 = delta2;
-        delta2 = w;
-
-        // 1. 挿入位置が削除範囲の中にある場合、挿入位置を削除範囲の開始地点にずらす。
-      } else if (this.isDeltaIncludePosition(delta1, delta2.start)) {
-        const d = this.calcMoveDelta(delta1.start, delta2.start);
-        delta2.start = delta1.start;
-        delta2.end = this.calcMovedPosition(delta2.end, d);
-      }
-
-      // 2. 挿入位置が削除範囲の後ろにある場合、そのまま返す。
-    }
-
-    return [delta1, delta2];
   }
-
 
   /**
    * 開始位置から終了位置までの、カーソル位置の差分を計算する。
